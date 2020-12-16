932a57029c642673fe5bd465b6274842e97ac4eb
Radhakrishnan-S-12 <srkrocky0@gmail.com>
2020-12-16 18:24:31 +0530

    Added service discovery

---
+++ eureka-server/src/test/java/com/autopsares/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopsares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
